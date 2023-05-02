e176bdcfab69d3e90a7b6475c2dcea7944cef184
evgenii klimashko <klimashko108@gmail.com>
2023-05-02 04:54:47 +0300

    Отладка обработки response

--- utils/helper.py
+++ utils/helper.py
@@ -1,3 +1,4 @@
+import json
 import logging
 
 import allure
@@ -14,10 +15,28 @@ class BaseSession(Session):
     def request(self, method, url, **kwargs):
         with step(f'{method} {url}'):
             response = super().request(method=method, url=f'{self.base_url}{url}', **kwargs)
+            content_type = response.headers.get('Content-Type')
             logging.info(f"Status code: {response.status_code} {curlify.to_curl(response.request)}")
             logging.info(response.text)
-            allure.attach(f"Status code: {response.status_code} {curlify.to_curl(response.request)}", name="Text",
-                          attachment_type=allure.attachment_type.TEXT)
-            allure.attach(response.text, name="HTTP Response",
-                          attachment_type=allure.attachment_type.TEXT)
+            logging.info(content_type)
+            #
+            allure.attach(f"Status code: {response.status_code} {curlify.to_curl(response.request)}", name="Text", attachment_type=allure.attachment_type.TEXT)
+            # allure.attach(response.text, name="HTTP Response",
+            #               attachment_type=allure.attachment_type.TEXT)
+            attachments_dict = {
+                'text/plain': (response.text, 'Text response', allure.attachment_type.TEXT),
+                'text/html': (response.text, 'Text response', allure.attachment_type.TEXT),
+                'application/json': (response.text, 'JSON response', allure.attachment_type.JSON),
+                'application/xml': (response.text, 'XML response', allure.attachment_type.XML),
+                'application/pdf': (response.content, 'PDF response', allure.attachment_type.PDF),
+                'image/jpeg': (response.content, 'Image response', allure.attachment_type.PNG),
+                'image/png': (response.content, 'Image response', allure.attachment_type.PNG),
+                'image/gif': (response.content, 'Image response', allure.attachment_type.PNG),
+            }
+            if content_type in attachments_dict.keys():
+                attachment_args = attachments_dict[content_type]
+                allure.attach(*attachment_args)
+            else:
+                allure.attach(response.content, 'Unknown response',
+                          allure.attachment_type.TEXT)
         return response
